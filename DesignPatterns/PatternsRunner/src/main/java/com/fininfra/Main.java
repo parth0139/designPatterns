24cad7f6e4eabfa3ed06fd6310ecf78fa9e9a94f
psroy0139 <parth.roy@pratishthanventures.com>
2025-08-16 02:03:52 +0530

    main class

--- DesignPatterns/PatternsRunner/src/main/java/com/fininfra/Main.java
+++ DesignPatterns/PatternsRunner/src/main/java/com/fininfra/Main.java
@@ -5,31 +5,29 @@ import java.util.logging.Logger;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
+    private static final String STARS= "**************************** \n";
 
     static {
         LoggerSetup.setupLogger();
     }
-    
+
     public static void main(String[] args) {
         logger.log(Level.INFO,"Starting Design Patterns Runner... \n");
 
-        StrategyPattern.run();
-
+        logger.log(Level.WARNING,"{0}Running Creational Patterns...\n{0}", STARS);
         Singleton.run();
-
-        ObserverPattern.run();
-
-        DecoratorPattern.run();
-
+        BuilderPattern.run();
         FactoryPattern.run();
-
         AbstractFactoryPattern.run();
+        PrototypePattern.run();
 
+        logger.log(Level.WARNING,"{0}Running Structural Patterns...\n{0}", STARS);
+        DecoratorPattern.run();
         ProxyPattern.run();
 
-        BuilderPattern.run();
-
-        PrototypePattern.run();
+        logger.log(Level.WARNING,"{0}Running Behavioural Patterns...\n{0}", STARS);
+        StrategyPattern.run();
+        ObserverPattern.run();
 
         logger.log(Level.INFO,"All patterns executed successfully...");
 
